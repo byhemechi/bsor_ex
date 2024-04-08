e191d05e8a4dd4945fb026dd35d5847123f682a8
George Fischer <me@george.id.au>
2024-04-08 15:36:05 +1000

    Publish to hex

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule Bsor.MixProject do
       app: :bsor,
       version: "0.1.0",
       elixir: "~> 1.16",
+      description: "Elixir parser for BeatLeader replays",
+      package: package(),
       deps: deps()
     ]
   end
@@ -20,8 +22,15 @@ defmodule Bsor.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/byhemechi/bsor_ex"}
     ]
   end
 end
